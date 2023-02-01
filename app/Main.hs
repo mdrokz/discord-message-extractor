0e5c3a0aa1566e173ce869e04226b3c939414724
mdrokz <mohammadmunshi@gmail.com>
2023-02-01 16:30:42 +0530

    chore: change method to return posix seconds

--- app/Main.hs
+++ app/Main.hs
@@ -14,6 +14,9 @@ import Data.String.Interpolate (i)
 
 -- Date Imports
 import Data.Time
+
+import Data.Time.Clock.POSIX
+
 import Data.Aeson
 
 version = "0.0.1"
@@ -22,8 +25,7 @@ help = "Usage: run --token <your discord token> --channel <discord channel> --li
 
 
 -- method to substract x days from today
-calculateDateTimeStamp :: NominalDiffTime -> UTCTime -> UTCTime
-calculateDateTimeStamp x k = addUTCTime (-86400 * x) k
+calculateDateTimeStamp x k = utcTimeToPOSIXSeconds $ addUTCTime (-86400 * x) k
                             
                                                         
 discordUrl :: Text -> Text -> Text -> String
@@ -34,13 +36,14 @@ main = getArgs >>= \case
   ["help"] -> putStrLn help
   ["run"] -> do
     t <- getCurrentTime
-    let x = calculateDateTimeStamp 200 t
+    let x = calculateDateTimeStamp 100 t
     --  x to string
-    let y = formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S UTC" x
-    let c = formatTime defaultTimeLocale "%s" x
+    -- 1064197824746623047
+    -- use formatTime to get long unix epoch
+    
     let z = decode "{\"id\": \"248186636381192193\", \"username\": \"md\", \"display_name\": null, \"avatar\": \"7a09670f05c69416ab37b4aae4522965\", \"avatar_decoration\": null, \"discriminator\": \"9742\", \"public_flags\": 4194432}" :: Maybe Author
     print z
-    putStrLn c
+    print x
     putStrLn "Running!"
     putStrLn $ discordUrl "123" "123" "123"
   ["version"] -> putStrLn version
