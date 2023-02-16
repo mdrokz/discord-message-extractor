08e27c0b07306c814b3eb6e10759096514489a00
mdrokz <mohammadmunshi@gmail.com>
2023-02-17 03:50:02 +0530

    feat: refactor logic to process based on msg id

--- app/Main.hs
+++ app/Main.hs
@@ -1,32 +1,28 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE DataKinds #-}
 
 module Main where
 
 -- Date Imports
 
+import Control.Concurrent.Async (mapConcurrently)
 import Control.Lens
-
-import Control.Concurrent.Async (async, wait)
-
 import Control.Monad
-import Data.Aeson (Value, decode, encode, ToJSON (toJSON))
+import Data.Aeson (ToJSON (toJSON), Value, decode, encode)
+import qualified Data.ByteString as BStr
 import Data.ByteString.Char8 as C8 (ByteString, pack)
 import qualified Data.ByteString.Lazy as LBS
 import Data.Map as Map
 import Data.String.Interpolate (i)
-import Data.Text (pack,replace,unpack)
-import Data.Time
-import Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
-import Discord (Author, Discord, Embed, Media , SimplifiedData,toSimplifiedData)
+import Data.Text (pack, replace, unpack)
+import Discord (Author, Discord (uId), Embed, Media, SimplifiedData (sId), toSimplifiedData)
 import Network.Wreq
 import System.Environment (getArgs)
-import qualified Data.ByteString as BStr
 
 version = "0.0.1"
 
@@ -34,17 +30,6 @@ help = "Usage: run -token <your discord token> -channel <discord channel> -limit
 
 stringToBS :: String -> ByteString
 stringToBS str = do C8.pack str
-
-removeChar :: Char -> String -> String
-removeChar c s = unpack (replace (Data.Text.pack [c]) mempty (Data.Text.pack s))
-
--- method to substract x days from today
-calculateDateTimeStamp d k =  addUTCTime (-86400 * d) k
-
-processTimestamps :: [UTCTime] -> UTCTime -> [UTCTime]
-processTimestamps [] _ = []
-processTimestamps (x:xs) k = calculateDateTimeStamp 100 k : (processTimestamps xs $ calculateDateTimeStamp 100 k)
-
 
 discordUrl :: String -> String -> String
 discordUrl channel limit = [i|https://discordapp.com/api/v9/channels/#{channel}/messages?limit=#{limit}|]
@@ -56,88 +41,48 @@ fetchData token url = do
   let opts = defaults & header "Authorization" .~ [stringToBS token]
   asJSON =<< getWith opts url :: IO DiscordRes
 
-processData t u = do
-  r <- fetchData t u
-  -- discord data to simplified data using fmap
-  let d = r ^. responseBody
+processData token ul = do
+  case ul of
+    (x : xs) -> do
+      r <- fetchData token x
 
-  let simplifiedDatas = fmap toSimplifiedData d
+      let d = r ^. responseBody
 
-  print "processing data\n"
-  print u
+      let simplifiedDatas = fmap toSimplifiedData d
 
-  pure simplifiedDatas
+      let lastMessage = sId $ Prelude.last simplifiedDatas
 
+      processedDatas <- mapConcurrently (process token lastMessage) xs
+
+      let t = d <> Prelude.concat processedDatas
+
+      pure $ fmap toSimplifiedData t
+
+  where 
+      process token id url = do
+        r <- fetchData token $ url ++ "&before=" ++ id
+        let d = r ^. responseBody
+        pure d
 main :: IO ()
 main =
   getArgs >>= \case
     ["help"] -> putStrLn help
     ["run", "--token", t, "--channel", c, "--limit", l] -> do
-      -- Ki.scoped \scope -> do 
-      --   Ki.
-      -- r <- fetchData t $ discordUrl c l
-
+    
       let lim = read l :: Int
 
       let messageLimit = div lim 100
 
       let threads = [1 .. messageLimit]
 
-      currentTime <- getCurrentTime
+      let values = fmap (\_ -> discordUrl c "100") threads
 
-      -- let d = discordUrl c 100
-      -- let gh = calculateDateTimeStamp 100 currentTime
-      -- let g = d ++ gh :: String
-
-      let actions = fmap (const currentTime) threads
-
-      --  removeChar 's' $ removeChar '.' $
-
-      let timestamps =  removeChar 's' . removeChar '.' . show . utcTimeToPOSIXSeconds <$> processTimestamps actions currentTime
-
-      -- print timestamps
-
-      let values = discordUrl c "100" : fmap (\x -> discordUrl c "100" ++ "&before=" ++ x) timestamps
-
-      threads <- mapM (async . processData t) values
-
-      results <- mapM wait threads
-
-      putStrLn $ "Array length: " ++ show (Prelude.length results)
-
-      let p = Prelude.take 12 $ Prelude.head results
+      p <- processData t values
 
       let r = LBS.toStrict $ encode p
 
-      -- write r to a json file
       BStr.writeFile "data.json" r
 
-      -- print $ Prelude.head results
-      print r
-
-      -- putStrLn $ "Array length: " ++ show (Prelude.length results)
-
-      -- Ki.scoped \scope -> do
-      --     currentTime <- getCurrentTime
-
-      --     pure ()
-
-      -- a <- getArgs
-      print t
-      print c
-      print l
-
-      t <- getCurrentTime
-      let x = calculateDateTimeStamp 100 t
-      --  x to string
-      -- 1064197824746623047
-      -- use formatTime to get long unix epoch
-
-      let z = decode "{\"id\": \"248186636381192193\", \"username\": \"md\", \"display_name\": null, \"avatar\": \"7a09670f05c69416ab37b4aae4522965\", \"avatar_decoration\": null, \"discriminator\": \"9742\", \"public_flags\": 4194432}" :: Maybe Author
-      print z
-      print x
-      putStrLn "Running!"
-      putStrLn $ discordUrl "123" "123"
     ["version"] -> putStrLn version
     _ -> do
-      putStrLn "Hello, Haskell!"
+      putStrLn "Enter a valid command!"
