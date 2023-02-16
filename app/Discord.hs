6b96528434e84d5ccaadf4f0c2b71de4b50dc751
mdrokz <mohammadmunshi@gmail.com>
2023-02-16 20:31:59 +0530

    feat: use proper types for parsing discord json

--- app/Discord.hs
+++ app/Discord.hs
@@ -3,111 +3,186 @@
 
 module Discord where
 
+import Control.Lens (without)
+import Data.Aeson
+import Data.String
 import GHC.Generics
 
-import Data.Text
-
-import Data.Aeson
-
-data Author = Author {
-    authorId :: Text,
-    username :: Text,
-    discriminator :: Text,
-    avatar :: Text,
+data Author = Author
+  { authorId :: String,
+    username :: String,
+    discriminator :: String,
+    avatar :: String,
     publicFlags :: Int
-} deriving (Show, Generic)
+  }
+  deriving (Show, Generic)
 
 instance ToJSON Author where
   toJSON = genericToJSON defaultOptions
 
 instance FromJSON Author where
-    parseJSON = withObject "Author" $ \v -> Author
-        <$> v.: "id"
-        <*> v.: "username"
-        <*> v.: "discriminator"
-        <*> v.: "avatar"
-        <*> v.: "public_flags"
+  parseJSON = withObject "Author" $ \v ->
+    Author
+      <$> v
+      .: "id"
+      <*> v
+      .: "username"
+      <*> v
+      .: "discriminator"
+      <*> v
+      .: "avatar"
+      <*> v
+      .: "public_flags"
 
-data EmbedData = EmbedData {
-    name :: Text,
-    url :: Text
-} deriving (Show, Generic)
+data EmbedData = EmbedData
+  { name :: String,
+    url :: Maybe String
+  }
+  deriving (Show, Generic)
 
 instance FromJSON EmbedData where
-  parseJSON = genericParseJSON defaultOptions
+  parseJSON = withObject "EmbedData" $ \v ->
+    EmbedData
+      <$> v
+      .: "name"
+      <*> v
+      .:? "url"
 
 instance ToJSON EmbedData where
   toJSON = genericToJSON defaultOptions
 
-data Media = Media {
-    height :: Int,
+data Media = Media
+  { height :: Int,
     width :: Int,
-    mediaUrl :: Text
-} deriving (Show, Generic)
+    mediaUrl :: String
+  }
+  deriving (Show, Generic)
 
 instance FromJSON Media where
-    parseJSON = withObject "Media" $ \v -> Media
-        <$> v.: "height"
-        <*> v.: "width"
-        <*> v.: "url"
+  parseJSON = withObject "Media" $ \v ->
+    Media
+      <$> v
+      .: "height"
+      <*> v
+      .: "width"
+      <*> v
+      .: "url"
 
 instance ToJSON Media where
   toJSON = genericToJSON defaultOptions
 
-data Embed = Embed {
-    embedtype :: Text,
-    embedUrl :: Text,
-    title :: Text,
-    description :: Text,
-    color :: Int,
-    embedAuthor :: EmbedData,
-    provider :: EmbedData,
-    thumbnail :: Media,
-    video :: Media
-} deriving (Show, Generic)
+data Embed = Embed
+  { embedType :: String,
+    embedUrl :: String,
+    title :: String,
+    description :: Maybe String,
+    color :: Maybe Int,
+    embedAuthor :: Maybe EmbedData,
+    provider :: Maybe EmbedData,
+    thumbnail :: Maybe Media,
+    video :: Maybe Media
+  }
+  deriving (Show, Generic)
 
 instance FromJSON Embed where
-    parseJSON = withObject "Embed" $ \v -> Embed
-        <$> v.: "type"
-        <*> v.: "url"
-        <*> v.: "title"
-        <*> v.: "description"
-        <*> v.: "color"
-        <*> v.: "author"
-        <*> v.: "provider"
-        <*> v.: "thumbnail"
-        <*> v.: "video"
+  parseJSON = withObject "Embed" $ \v ->
+    Embed
+      <$> v
+      .: "type"
+      <*> v
+      .: "url"
+      <*> v
+      .: "title"
+      <*> v
+      .:? "description"
+      <*> v
+      .:? "color"
+      <*> v
+      .:? "author"
+      <*> v
+      .:? "provider"
+      <*> v
+      .:? "thumbnail"
+      <*> v
+      .:? "video"
+
+-- specify optional field in json
+-- https://stackoverflow.com/questions/44675309/optional-field-in-aeson
 
 instance ToJSON Embed where
   toJSON = genericToJSON defaultOptions
 
-
-data Discord = Discord {
-    id :: Text,
+data Discord = Discord
+  { id :: String,
     messageType :: Int,
-    content :: Text,
-    channelId :: Text,
+    content :: String,
+    channelId :: String,
     author :: Author,
-    attachments :: [Text],
+    attachments :: [String],
     embeds :: [Embed],
     pinned :: Bool,
     tts :: Bool,
-    timestamp :: Text
-} deriving (Show, Generic)
-
+    timestamp :: String
+  }
+  deriving (Show, Generic)
 
 instance FromJSON Discord where
-    parseJSON = withObject "Discord" $ \v -> Discord
-        <$> v.: "id"
-        <*> v.: "type"
-        <*> v.: "content"
-        <*> v.: "channel_id"
-        <*> v.: "author"
-        <*> v.: "attachments"
-        <*> v.: "embeds"
-        <*> v.: "pinned"
-        <*> v.: "tts"
-        <*> v.: "timestamp"
+  parseJSON = withObject "Discord" $ \v ->
+    Discord
+      <$> v
+      .: "id"
+      <*> v
+      .: "type"
+      <*> v
+      .: "content"
+      <*> v
+      .: "channel_id"
+      <*> v
+      .: "author"
+      <*> v
+      .: "attachments"
+      <*> v
+      .: "embeds"
+      <*> v
+      .: "pinned"
+      <*> v
+      .: "tts"
+      <*> v
+      .: "timestamp"
 
 instance ToJSON Discord where
-    toJSON = genericToJSON defaultOptions
+  toJSON = genericToJSON defaultOptions
+
+data SimplifiedData = SimplifiedData
+  { sAuthor :: Maybe EmbedData,
+    sContent :: String,
+    sDescription :: Maybe String,
+    sEmbed :: String,
+    sTitle :: String,
+    sThumbnail :: String,
+    sTimestamp :: String
+  }
+  deriving (Show, Generic)
+
+instance ToJSON SimplifiedData where
+  toJSON (SimplifiedData a c d e t th ts) =
+    object
+      [ "author" .= a,
+        "content" .= c,
+        "description" .= d,
+        "type" .= e,
+        "title" .= t,
+        "thumbnail" .= th,
+        "timestamp" .= ts
+      ]
+
+
+toSimplifiedData discord = SimplifiedData
+                { sAuthor = (embedAuthor . Prelude.head . embeds) discord,
+                  sContent = content discord,
+                  sDescription = (description . Prelude.head . embeds) discord,
+                  sEmbed = (embedType . Prelude.head . embeds) discord,
+                  sTitle = (title . Prelude.head . embeds) discord,
+                  sThumbnail = maybe "" mediaUrl ((thumbnail . Prelude.head . embeds) discord),
+                  sTimestamp = timestamp discord
+                }
